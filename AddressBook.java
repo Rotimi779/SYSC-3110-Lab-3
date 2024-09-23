257347d1fa100349b4547d9e8a11ba42a9491ee9
Rotimi779 <tomiwaajayi85@yahoo.com>
2024-09-23 11:57:35 -0400

    Made an address book and added a buddy. Removed buddy from address book.

--- AddressBook.java
+++ AddressBook.java
@@ -14,17 +14,18 @@ public class AddressBook  {
         }
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-        buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index) {
+        if ( index >= 0 && index < buddies.size()){
+           return buddies.remove(index);
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-
-        AddressBook addressBook = new AddressBook();
-        System.out.println("Address Book");
         BuddyInfo buddy = new BuddyInfo("Rotimi","Revalie",101253689);
         AddressBook addressBook1 = new AddressBook();
         addressBook1.addBuddy(buddy);
-        addressBook1.removeBuddy(buddy);
+        addressBook1.removeBuddy(0);
+        System.out.println("New change");
     }
 }
